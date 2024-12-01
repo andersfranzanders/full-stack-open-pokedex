7aa1fc728c7c38e875ac6ad8cbaaafe7486d5621
Franz Anders <franz.anders.fa@gmail.com>
2024-12-01 15:50:32 +0100

    exercise 11.9.b ... e2etest should run?

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,12 @@
-import { defineConfig } from '@playwright/test';
-export default defineConfig({
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './e2e-tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
   webServer: {
     command: 'npm start',
     url: 'http://127.0.0.1:8080',
@@ -8,6 +15,24 @@ export default defineConfig({
   },
   use: {
     baseURL: 'http://localhost:8080/',
-    browserName: 'chromium', // Default browser to use
+    headless: true,
+    browserName: 'chromium',
+    trace: 'on-first-retry',
   },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    } 
+  ]
 });
